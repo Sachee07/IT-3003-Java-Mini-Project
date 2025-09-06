45581a6587dd03331900ecc20d65d7ef4d389390
Sachee07 <sachee789@gmail.com>
2025-09-06 20:58:25 +0530

    jdbc driver added

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
     private static final String USER = "root";   
-    private static final String PASSWORD = "root"; 
+    private static final String PASSWORD = "828964";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
